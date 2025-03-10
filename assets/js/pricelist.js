9bf5b74d1fbb29aed8305bca5c414f0ac3968c76
Atupele Mulimbika <atupelemulimbika@gmail.com>
2025-03-10 13:16:46 +0200

    Update pricelist.js

--- assets/js/pricelist.js
+++ assets/js/pricelist.js
@@ -16,7 +16,7 @@ const priceList = [
         title: "Dreads",
         filter:"shaving",
         picture: "./assets/images/pricing-2.jpg",
-        text: `-Dreads (retouch) 200 <br> -Sister locs (start) 1500 <br> -Sister locs (Retouch) 4 to 6 weeks 500
+        text: `-Dreads (retouch) 150 <br> -Sister locs (start) 1500 <br> -Sister locs (Retouch) 4 to 6 weeks 500
                  <br> -Sister locs 6 to 7 weeks 600 <br> -Above 7 weeks 800`
     },
     {
@@ -34,7 +34,7 @@ const priceList = [
         filter:"body-treatments",
         picture: "./assets/images/pricing-4.jpg", 
         text: `-3 Strands braids (plain) 250
-        <br> -Flat twist 200 <br> -Flat twist (with extensions) 350`
+        <br> -Flat twist 250 <br> -Flat twist (with extensions) 350`
     },
     {   
         serviceName:'beautySpa',
@@ -42,7 +42,7 @@ const priceList = [
         filter:"beauty-spa",
         picture: "./assets/images/pricing-5.jpg",
         title: "Hair styles",
-        text: `-Cornrows with natural hair 150 <br>-Cornrows with wigs 250<br>Stitch braids 350<br>-Natural twist 200<br>-Natural twist with extensions 450 <br>-Wash and blow 150 <br>-Unplaiting(Natural hair) 50 <br>-Unplaiting (Braids) 120 <br>-Knotless Twist 500 <br>-Knotless braids (normal length) 500 <br>-Knotless (bum length) 600 <br>-Knotless braids (extra long) 700 <br>-Micro Knotless (normal length) 650 <br>-Micro knotless (bum length) 750 <br>-Butterfly locs (normal length) 600 <br>-Butterfly locs (long) 750 <br>-Faux locs (normal length) 650 <br>-Faux locs (long) 750 <br>-Fishtail push back 300 <br>-Fish tail (other hair styles) 450 <br>-Box braids (normal length) 500 <br>-Box braids (with cornrows) 400 <br>-Weave in 450 <br>-Spanish curl 850 <br>-Goddess braids 650 <br>-Afro kinky 500 <br>-Passion Twist 600 <br>-Freetrees Micro (wigs inclusive) 750
+        text: `-Cornrows with natural hair 150 <br>-Cornrows with wigs 250<br>Stitch braids 350<br>-Natural twist 250<br>-Natural twist with extensions 550 <br>-Wash and blow 150 <br>-Unplaiting and detangling(Natural hair) 50 <br>-Unplaiting and detangling (Braids) 120 <br>-Knotless Twist 500 <br>-Knotless braids (normal length) 500 <br>-Knotless (bum length) 600 <br>-Knotless braids (extra long) 700 <br>-Micro Knotless (normal length) 650 <br>-Micro knotless (bum length) 750 <br>-Butterfly locs (normal length) 600 <br>-Butterfly locs (long) 750 <br>-Faux locs (normal length) 650 <br>-Faux locs (long) 750 <br>-Fishtail push back 300 <br>-Fish tail (other hair styles) 450 <br>-Box braids (normal length) 500 <br>-Box braids (with cornrows) 400 <br>-Weave in 450 <br>-Spanish curl 750 <br>-Goddess braids 650 <br>-Afro kinky 500 <br>-Passion Twist 550 <br>-Freetrees Micro (wigs inclusive) 750
         <br>-Freetrees Micro (own wigs) 500 <br>-Jungle Braids 600 <br> Box braids twist`,
     },
     // {
